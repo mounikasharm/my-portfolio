3dc80173b032a157d7b23e7e949506f2913042be
Mounika <your-email@example.com>
2025-12-04 16:22:18 +0530

    updated home page design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,8 +30,8 @@ export default function AboutSection() {
     },
 
     profileImage: {
-      width: "100%",
-      height: "100%",
+      width: "70%",
+      height: "70%",
       borderRadius: "50%",
       objectFit: "cover",
     },
